8f008a660d4ccb1cade83610f85c732b06f46448
gijoeuk <josephcingram@hotmail.co.uk>
2017-08-21 12:14:56 +0100

    Add refactored code for fizzbuzz method and fizzbuzz_spec test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
 
 require './lib/fizzbuzz'
 describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).eql? 'fizzbuzz'
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).eql? 'buzz'
+  end
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).eql? 'fizz'
   end
 end
--- spec/lib/fizzbuzz.rb
+++ spec/lib/fizzbuzz.rb
@@ -1,6 +1,7 @@
 
 def fizzbuzz(number)
-  return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
-  return 'fizz' if number % 3 == 0
-  return 'buzz' if number % 5 == 0
+  return "fizzbuzz" if number % 3 == 0 && number % 5 == 0
+  return "fizz" if number % 3 == 0
+  return "buzz" if number % 5 == 0
+  number 
 end
